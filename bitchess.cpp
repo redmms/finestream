18f1d133dc5b96e6af90309ccfa852d68d735f8d
redmms <101011989+redmms@users.noreply.github.com>
2023-06-26 21:10:44 +0600

    Include all possible types for output

--- bitchess.cpp
+++ bitchess.cpp
@@ -132,6 +132,27 @@ public:
 			cout << "Error in PutByte(): 108 line - the output byte isn't left aligned" << endl;
 		}
 	}
+	template <typename T>
+	inline void PutAny(T value) { // works with double but can potentially reverse other types/structures
+		uint8_t* bytePtr = reinterpret_cast<uint8_t*>(&value);
+		for (int i = sizeof(value) - 1; i >= 0; --i) {
+			fileStream.put(static_cast <uint8_t> (bytePtr[i]));
+		}
+	}
+	template <typename T>
+	inline void PutAnyReversed(T value) {
+		uint8_t* bytePtr = reinterpret_cast<uint8_t*>(&value);
+		for (int i = 0, size = sizeof(value); i < size; ++i) {
+			fileStream.put(static_cast <uint8_t> (bytePtr[i]));
+		}
+	}
+	template <typename T>
+	void ToBytes(const T & value, uint8_t * bytesArray) {
+		uint8_t* bytePtr = reinterpret_cast<uint8_t*>(&value);
+		for (int i = sizeof(value) - 1; i >= 0; --i) {
+			bytesArray[i] = bytePtr[i];
+		}
+	}
 	BitStream& operator << (const bitset <8> & boardLine) {
 		PutByte(boardLine);
 		return *this;
@@ -167,7 +188,7 @@ public:
 			}
 		}
 		if (RPZSIZE) {  // output right puzzle // should be after if (N >= 8) otherwise output order will be wrong
-			bitset <8> bsRightPuzzle(strSet, RPZLB, N); // 
+			bitset <8> bsRightPuzzle(strSet, RPZLB, N);
 			BitRemedy brRightPuzzle{ bsRightPuzzle, RPZSIZE, false };
 			brLastByte = brRightPuzzle.MoveToLeft();
 		}
@@ -206,57 +227,94 @@ public:
 			return *this;
 	}
 	template <typename T>
-	BitStream& operator << (const T & type) {
-		if (is_integral<T>::value)
-			fileStream << static_cast <T> (type);
+	BitStream& operator << (const T& type) {
+		if constexpr (Container <T>) {
+			for (auto& element : type) {
+				*this << element;
+			}
+		}
+		else if constexpr (is_array_v <T>) {
+			for (int i = 0, size = sizeof(type) / sizeof(type[0]); i < size; i++) {
+				*this << type[i];
+			}
+		}
+		else if constexpr (sizeof(T) == 1) {
+			PutByte(type);
+		}
+		else if constexpr (is_integral_v <T>) {
+			PutAny(type);
+		}
 		else {
-			fileStream << static_cast <T> (type);
-			cout << "Warning: are you sure about this type - " << typeid(T).name() << "?" << endl; // do we really need this constraint? 
+			PutAny(type);
+			cout << "Are you sure about this type - " << typeid(T).name() << "?" << endl;
 		}
 		return *this;
 	}
-	template <typename T>
-		requires Container<T>
-	BitStream& operator << (const T & type) {
-		for (auto& element : type) {
-			*this << element;
-		}
-		return *this;
-	}
-	// TODO: добавить определение оператора >>
-	// TODO: заменить cout << "Error"; на throw exception
-	// TODO: переработать bitset(str,,)
-	// TODO: пересмотреть size_t шаблоны
+	// TODO: add >> operator definition
+	// TODO: replace cout << "Error"; by throw exception or some warnings
+	// TODO: correct bitset(str,,) parameteres
+	// TODO: revise size_t templates
+	// TODO: add saving stream in array/pack, prepared to be sent, and than add move semantics for this pack
+	// TODO: add saving stream in another representable view: some analog or extension of vector <bool> / bitset to store in bits, but show in chars '1' or '0'
+	// TODO: add modules
+	// TODO: add check of bitsN range 1-7 for BitRemedy constructor
 };
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
 int main() {
-	//int n = 5;
-	//bitset <8> a;
-	//a = 0b100;
-	//a[1] = 1;
-	//cout << a << endl;
-	//a = 0b010;
-	//cout << a;
-	//cout << endl;
-	//Board myBoard(1);
-	//cout << myBoard[0] << endl;
-	//array <uint8_t, 1> emptyArr;
-	//std::cout << "Size of array with one uint8_t: " << sizeof(emptyArr) << " bytes" << std::endl;
-	//bitset <7> bSet7;
-	//std::cout << "Size of bitset 7: " << sizeof(bSet7) << " bytes" << std::endl;
-	//uint8_t i8;
-	//cout << "Size of uint8_t: " << sizeof(i8) << " bytes" << endl;
-	//uint8_t * arr2 = new uint8_t[2]{ 1, 1 };
-	//cout << "Size of massive with two uint8_t: " << sizeof(arr2) << " bytes" << endl;
-	//bitset <8> s7(0b01010101);
-	//i8 = static_cast <uint8_t> (s7.to_ulong());
-	//cout << int(i8) << endl;
-	//uint8_t* dynamicArray = new uint8_t[100];
-	//cout << sizeof(dynamicArray) << endl;
-	//vector <string> v(10);
-	//cout << sizeof(v);
-	fstream bsm("output.txt");
-	BitRemedy a{ pow(2, 7), 7, true}, b{ 0b10101010, 7, true }, c{ 0b11111111, 7, true };
-	BitRemedy arr[3]{ a,b,c };
-	double darr[]{ 12, 14 };
-	bsm.write(12); bsm.write/ reinterpret_cast<const char*> (14);
+	BitStream bsm("output.txt"); // struct, union
+	long long O = 0; // separator
+	int i = 3;
+	char ci = 3; // same as i, it was hard, but I managed to avoid all fstream casts from int to char, double to char etc.
+	char cc = '3';
+	double d = 12;
+	double dpoint = 3.14;
+	double dpointm = -3.14;
+	int carr[3]{ 1, 2, 3 };
+	array<int, 5> cpparr{ 1, 2, 3, 4, 5 };
+	tuple<int, double, char> tup{ 1, 3.14, 'a' }; // includes 2 bytes of gurbage
+	tuple<long long, double, char> lltup{ 1, 3.14, 'a' };
+	pair<int, double> pair{ 1, 3.14 };
+	bitset <8> bs (7);
+	bitset <18> bsn(pow(2, 17) + pow(2, 15) + pow(2, 13) + 3);
+	BitRemedy bra{ 0b00000111, 3, false }, brb{ 0b00000111, 3, true }, brc{ 0b00000111, 7, true }, brd{ 0, 7, false };
+	brd.bsByte.set(7), brd.bsByte.set(6), brd.bsByte.set(5);	
+	bool b = true;
+	vector < bool> vb(13);
+	for (auto el : { 0, 12, 2, 4 }) {
+		vb[el] = 1;
+	}
+	deque <int> dq{ 7, 6, 5 }; // fine, it's a sequence container
+	queue <int> q;  // oops, seems you use a container adaptor
+	priority_queue <char> pq; // oops, seems you use a container adaptor
+	stack <int> st;  // oops, seems you use a container adaptor
+	for (auto el : { 7, 6, 5 }) {
+		q.push(el);
+		pq.push(el);
+		st.push(el);
+	}
+	int* p = &i; // 8 bytes (can differ for another proccessor architecture), even more than in a usual int (4 bytes)
+	int& r = i; // will output value of i, but you can use << &r and it will have the same type as p
+	// these two values will differ for every program run like the address of i;
+	// bsm << &r << O << p;
+	union MyUnion { char u0; uint16_t u1[2]; int u2; };
+	MyUnion u{ 0xFFFFFFFF };
+	//bsm.PutAnyReversed(u);
+	u.u0 = 7;
+	u.u1[0] = 6;
+	u.u1[1] = 6;
+	u.u2 = 5;
+	/*bsm << u.u0
+		<< O << O
+		<< u.u1
+		<< O << O
+		<< u.u2
+		<< O << O;*/ // usable, but no guarantees 
+	struct MyStruct { char u0; uint16_t u1[2]; int u2; };
+	MyStruct s{ 7, {6, 6}, 5 }; // usable, but no guarantees
 }
+// it is not the best BitStream, it isn't super mega duper or even unic, it just works and works FINE
